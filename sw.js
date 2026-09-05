24cb444df93729981e95c800c47ccb4b877502e8
hstracker <hstracker@users.noreply.github.com>
2026-09-06 00:30:43 +0800

    iOS快捷指令截图自动上传PWA:Web Share Target+SW缓存+Tesseract OCR识别随从+我方/对手阵容显示

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@
  */
 const VERSION = 'hs-tracker-v1';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
+const SHARED_CACHE = 'hs-shared-shots';
+const SHARED_KEY = location.origin + location.pathname + 'shared-shot';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -26,6 +28,39 @@ self.addEventListener('fetch', function (event) {
   var origin = url.origin;
   var host = url.hostname;
   var isShell = origin === location.origin;
+
+  /* Web Share Target：iOS 快捷指令截图分享到 PWA，SW 拦截 POST 缓存图片后 303 回页面 */
+  if (isShell && event.request.method === 'POST' && url.searchParams.get('share') === '1') {
+    event.respondWith(
+      event.request.formData().then(function (fd) {
+        var file = fd.get('screenshot');
+        if (file && file.size > 0) {
+          return caches.open(SHARED_CACHE).then(function (cache) {
+            return cache.put(SHARED_KEY, new Response(file, { headers: { 'Content-Type': file.type || 'image/png' } }));
+          }).then(function () {
+            return Response.redirect('./?shared=1', 303);
+          });
+        }
+        return Response.redirect('./?shared=0', 303);
+      }).catch(function () {
+        return Response.redirect('./?shared=0', 303);
+      })
+    );
+    return;
+  }
+
+  /* 页面读取分享截图：返回 SW 缓存的图片 */
+  if (isShell && url.pathname.endsWith('shared-shot')) {
+    event.respondWith(
+      caches.open(SHARED_CACHE).then(function (cache) {
+        return cache.match(SHARED_KEY).then(function (hit) {
+          return hit || new Response('not found', { status: 404 });
+        });
+      })
+    );
+    return;
+  }
+
   var isWhiteCdn = host.indexOf('aka.doubaocdn.com') > -1 ||
                    host.indexOf('feishucdn.com') > -1 ||
                    host.indexOf('byteimg.com') > -1 ||
